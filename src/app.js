5553c60136861b2066044a716028ce9335d8d2f8
nicolett10 <torrestapia.n@gmail.com>
2022-08-31 23:41:36 +0000

    domain

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import "bootstrap";
+/*import "bootstrap";*/
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let Domain = () => {
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+    let dom = [".com", ".us", ".net", ".cl"];
+    let dominio = [];
+
+    for (let p = 0; p < pronoun.length; p++) {
+      for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < noun.length; n++) {
+          for (let d = 0; d < dom.length; d++)
+            dominio.push(pronoun[p] + adj[a] + noun[n] + dom[d]);
+        }
+      }
+    }
+
+    return dominio;
+  };
+  console.log(Domain());
 };
